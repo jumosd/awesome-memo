38142e3ec39a455dd94545d5d1d77bc041397b3b
jumosd <jumosd@icloud.com>
2023-06-30 18:45:31 +0900

    1

--- main.py
+++ main.py
@@ -2,6 +2,26 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
-app = FastAPI()
+#리퀘스트 바디로 들어온 객체의 형식을 지정해줌
+class Memo(BaseModel):
+    id : str 
+    content : str
+
+memos =[]
+
+app = FastAPI() 
+
+
+@app.post("/memos")
+def create_memo(memo:Memo):
+    memos.append(memo)
+    print(memos)
+    return "메모추가 성공했습니다."
+
+@app.get('/memos')
+def read_memo():
+    return memos
+
+
 
 app.mount("/", StaticFiles(directory='static', html=True), name = 'static')
--- static/app.js
+++ static/app.js
@@ -1,18 +1,47 @@
-//메모생성하기
-function createMemo(value){
-
+function displayMemos(memo) {
+  const ul = document.querySelector("#memo-ul");
+  const li = document.createElement("li");
+  li.innerText = `[id: ${memo.id}] ${memo.content}`;
+  ul.append(li);
 }
 
+// 메모 읽기
+async function readMemo() {
+  const res = await fetch("/memos");
+  const jsonRes = await res.json();
+  const ul = document.querySelector("#memo-ul");
+  ul.innerHTML = "";
+  jsonRes.forEach(displayMemos);
+}
+
+//메모만들어서 서버에 전송
+async function createMemo(value) {
+  const res = await fetch("/memos", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: new Date().getTime(),
+      content: value,
+    }),
+  });
+  const jsonRes = await res.json();
+  console.log(jsonRes);
+
+  readMemo();
+}
 
 //제출핸들러
 function handleSubmit(event) {
   event.preventDefault();
-  const input = document.querySelector('#memo-input');
-  console.log(input.value)
-  createMemo(input.value)
+  const input = document.querySelector("#memo-input");
+  console.log(input.value);
+  createMemo(input.value);
   input.value = "";
-
 }
 
 const form = document.querySelector("#memo-form");
 form.addEventListener("submit", handleSubmit);
+
+readMemo();
